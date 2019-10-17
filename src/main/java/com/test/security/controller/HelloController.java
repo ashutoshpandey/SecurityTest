2eff0e0787818786e7320974d658c96f1f9f6a3a
Ashutosh Pandey <ashutosh@keriton.com>
2019-10-17 17:54:03 +0530

    First commit

---
+++ src/main/java/com/test/security/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.test.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable("name") String name){
+        return "Hello " + name;
+    }
+}
